4ad98c59bec5c85e1e7df92415c2971617eaca5f
h3x <robert.h3x@gmail.com>
2015-09-28 16:35:47 +0200

    add git config

---
+++ git_config.sh
@@ -0,0 +1,7 @@
+git config --global user.email "robert.h3x@gmail.com"
+git config --global user.email "robert.h3x@gmail.com"
+
+git remote add origin ssh://github.com/h3x89/nagios
+
+git config --global core.editor "vim"
+
